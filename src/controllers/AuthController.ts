8ddbcce370b53f94028ac3c1d2684d3603bf68b6
fabiosntmirs <gmsantos1220@gmail.com>
2025-04-26 18:41:42 -0300

    ✨ feat: atualiza mensagem enviada ao front-ent no caso do login.

--- src/controllers/AuthController.ts
+++ src/controllers/AuthController.ts
@@ -1,20 +1,27 @@
-import { FastifyRequest, FastifyReply } from "fastify";
-import {AuthService} from "../services/AuthService"
+import { FastifyRequest, FastifyReply } from 'fastify';
+import { AuthService } from '../services/AuthService';
 
+class AuthController {
+  async handle(request: FastifyRequest, reply: FastifyReply) {
+    const { email, senha } = request.body as { email: string; senha: string };
+    const authService = new AuthService();
 
-class AuthController{
+    try {
+      const response = await authService.execute({ email, senha });
 
-
-    async handle(request: FastifyRequest, reply: FastifyReply){
-
-        const {email,senha} = request.body as {email: string,senha: string}
-        const authService = new AuthService()
-
-        const response = await authService.execute({email,senha})
-
-        reply.send("Login feito com sucesso!")
-
+      // sucesso em formato JSON
+      reply.send({ message: 'Login feito com sucesso!', data: response });
+    } catch (error) {
+      // Verifica se o erro é uma instância de Error
+      if (error instanceof Error) {
+        // Caso ocorra um erro, retorna o erro em formato JSON
+        reply.status(401).send({ message: 'Login falhou!', error: error.message });
+      } else {
+        // Caso o erro não seja uma instância de Error
+        reply.status(500).send({ message: 'Erro desconhecido!', error: 'Ocorreu um erro inesperado.' });
+      }
     }
+  }
 }
 
-export {AuthController}
+export { AuthController };
